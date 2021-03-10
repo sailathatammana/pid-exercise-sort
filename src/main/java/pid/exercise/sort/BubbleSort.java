ab4160da4792c096dad7d9a6eef2fefc7a599bd6
sailathatammana <sailathatammana1997@gmail.com>
2021-03-10 11:54:47 +0100

    Added method implementations

--- src/main/java/pid/exercise/sort/BubbleSort.java
+++ src/main/java/pid/exercise/sort/BubbleSort.java
@@ -1,9 +1,26 @@
 package pid.exercise.sort;
 
+import java.util.Arrays;
+
 public class BubbleSort {
 
     public static void sort(int[] array) {
         // TODO Implement bubble sort
+        int n = array.length;
+        for (int i = 1; i < n; i++)
+            for (int j = 0; j < n-1; j++) {
+                if (array[j] > array[j + 1]) {
+                    // swap arr[j+1] and arr[j]
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }   
+            }
     }
-
+    public static void main(String[] args){
+        BubbleSort ob = new BubbleSort();
+        int array[] = {23,12,11,1};
+        ob.sort(array);
+        System.out.println(Arrays.toString(array));
+    }
 }
--- src/main/java/pid/exercise/sort/SelectionSort.java
+++ src/main/java/pid/exercise/sort/SelectionSort.java
@@ -1,9 +1,32 @@
 package pid.exercise.sort;
 
+import java.util.Arrays;
+
 public class SelectionSort {
 
     public static void sort(int[] array) {
         // TODO Implement selection sort
+        int n = array.length;
+        // Outer array
+        for (int i = 0; i < n-1; i++)
+        {
+            // Find the minimum element in unsorted array
+            int min = i;
+            for (int j = i+1; j < n; j++) {
+                if (array[j] < array[min])
+                    min = j;
+            }
+            // Swap the found minimum element with the first
+            // element
+            int temp = array[min];
+            array[min] = array[i];
+            array[i] = temp;
+        }
     }
-
+    public static void main(String[] args){
+        SelectionSort ob = new SelectionSort();
+        int array[] = {23,11,12,1};
+        ob.sort(array);
+        System.out.println(Arrays.toString(array));
+    }
 }
